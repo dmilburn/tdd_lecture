e0c5048a7d157bd7e6b5c48aa696f6a42802864f
Debbie Milburn <debbiemilburn2@gmail.com>
2017-03-27 15:29:27 -0400

    Update tests

--- prime_numbers_spec.rb
+++ prime_numbers_spec.rb
@@ -1,28 +1,6 @@
 require_relative 'prime_numbers'
 require 'rspec'
 
-
-describe "prime_numbers" do
-  before(:all) do
-    @mixed_numbers = [1,2,3,4,5,6,7,8,9]
-    @even_numbers = [2,4,6]
-  end
-
-  it "should return an array" do
-    expect(prime_numbers(@mixed_numbers)).to be_an_instance_of(Array)
-
-  end
-
-  it "should return an array including only prime numbers" do
-    expect(prime_numbers(@mixed_numbers)).to include(3,5,7)
-  end
-
-  it "should return an empty array if there are no prime numbers" do
-    expect(prime_numbers(@even_numbers)).to be_empty
-  end
-
-end
-
 describe "is_prime?" do
   it "should return true if the number is a prime" do
     expect(is_prime?(3)).to be(true)
@@ -36,3 +14,26 @@ describe "is_prime?" do
     expect(is_prime?(6)).to be(false)
   end
 end
+
+
+describe "prime_numbers" do
+  before(:all) do
+    @mixed_numbers = [1,2,3,4,5,6,7,8,9,10000,35,21]
+    @even_numbers = [4,6,8]
+  end
+
+  it "should return an array" do
+    expect(prime_numbers(@mixed_numbers)).to be_an_instance_of(Array)
+
+  end
+
+  it "should return an array including only prime numbers" do
+    expect(prime_numbers(@mixed_numbers)).to contain_exactly(2,3,5,7)
+  end
+
+  it "should return an empty array if there are no prime numbers" do
+    expect(prime_numbers(@even_numbers)).to be_empty
+  end
+
+end
+
