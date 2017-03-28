c6078c94e08e534875b5cbe28dead33398eeda6c
Debbie Milburn <debbiemilburn2@gmail.com>
2017-03-28 09:26:51 -0400

    Refactor prime_numbers

--- prime_numbers.rb
+++ prime_numbers.rb
@@ -1,10 +1,9 @@
 def is_prime?(number)
   return false if number == 1
   return true if number == 2
-  (2...number).each do |n|
-    return false if number % n == 0
+  (2...number).none? do |n|
+    number % n == 0
   end
-  return true
 end
 
 def prime_numbers(numbers)
